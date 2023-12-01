f229e01c7b32e655bc9565518596bbf8cbae19a0
Greta Savio <g.savio@itsrizzoli.it>
2023-12-01 15:48:01 +0100

    raggruppiamo i punti!

--- creazione_record.py
+++ creazione_record.py
@@ -1,23 +1,39 @@
-# Qua abbiamo intenzione di creare i campi da inserire nel db
-# stiamo creando dei lotti utilizzando google heart
-# dobbiamo fare in modo che riusciamo ad inserire tutto
+from pykml import parser
+import re
+import pandas as pd
 
-import simplekml
-import xml.etree.ElementTree as ET
-
-def leggi_file_kml(nome_file):
-    kml = simplekml.Kml()
+def extract_data(placemark, data_dict):
+    name = placemark.find(".//kml:name", namespaces=namespace)
+    longitude = placemark.find(".//kml:LookAt/kml:longitude", namespaces=namespace)
+    latitude = placemark.find(".//kml:LookAt/kml:latitude", namespaces=namespace)
     
-    with open(nome_file, 'r', encoding='utf-8') as kml_file:
-        kml_string = kml_file.read()
-        root = ET.fromstring(kml_string)
-        kml.from_string(ET.tostring(root, encoding='utf8').decode('utf8'))
-    
-    for placemark in kml.features():
-        # Qui puoi accedere ai dati del placemark e fare quello che serve
-        print("Nome:", placemark.name)
-        print("Coordinate:", placemark.geometry.coords)
+    if name is not None:
+        name_text = name.text
+        if name_text not in data_dict:
+            data_dict[name_text] = {'index': len(data_dict) + 1, 'data': []}
+        data_entry = {'longitude': float(longitude.text), 'latitude': float(latitude.text)}
+        data_dict[name_text]['data'].append(data_entry)
 
-if __name__ == "__main__":
-    nome_file_kml = r".\coordinate_2.kml"
-    leggi_file_kml(nome_file_kml)
+def parse(root, data_dict):
+    for elt in root.getchildren():
+        tag = re.sub(r'^.*\}', '', elt.tag)
+        if tag in ["Document", "Folder"]:
+            parse(elt, data_dict)
+        elif tag == "Placemark":
+            extract_data(elt, data_dict)
+
+if __name__ == '__main__':
+    file_path = r"./coordinate_2.kml"
+    with open(file_path, 'r') as f:
+        root = parser.parse(f).getroot()
+
+    namespace = {"kml": 'http://www.opengis.net/kml/2.2'}
+    
+    data_dict = {}  # Dizionario per memorizzare i dati
+    parse(root, data_dict)
+
+    # Stampare il risultato
+    for name, data in data_dict.items():
+        print(f"Nome {name} (Index {data['index']}):")
+        for entry in data['data']:
+            print(f"  Longitude: {entry['longitude']}, Latitude: {entry['latitude']}")
