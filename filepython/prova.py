0bbe68a47487cf4868ccffd0e1da045296670009
Greta Savio <g.savio@itsrizzoli.it>
2023-12-04 17:50:04 +0100

    GUI - modificata

--- filepython/prova.py
+++ filepython/prova.py
@@ -1,33 +1,55 @@
 import tkinter as tk
 from tkinter import ttk
-from tkinterhtml import HtmlFrame
 import folium
+from screeninfo import get_monitors
 
 def on_button_click():
-    # Crea una mappa centrata su una determinata posizione
-    m = folium.Map(location=[41.9028, 12.4964], zoom_start=10)
+    ...
 
-    # Aggiunge un marcatore alla mappa
-    folium.Marker(location=[41.9028, 12.4964], popup="Roma").add_to(m)
+def on_search_button_click():
+    # Implementa la logica per cercare il nome
+    # Aggiungi il codice necessario qui
+    pass
 
-    # Salva la mappa in un file HTML temporaneo
-    m.save("map.html")
-
-    # Crea una finestra per visualizzare la mappa
-    map_window = tk.Toplevel(root)
-    
-    # Carica l'HTML nella finestra utilizzando tkinterhtml
-    html_frame = HtmlFrame(map_window)
-    html_frame.set_content("<iframe src='map.html' width='100%' height='100%'></iframe>")
-    html_frame.pack(fill=tk.BOTH, expand=True)
+def on_exit_button_click():
+    root.destroy()
 
 # Creazione della finestra principale
 root = tk.Tk()
-root.title("Simulazione Pagina Software con Mappa")
+root.title("CATASTO")
+
+# Centra la finestra principale
+window_width = 400
+window_height = 300
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+x_position = (screen_width - window_width) // 2
+y_position = (screen_height - window_height) // 2
+root.geometry(f"{window_width}x{window_height}+{x_position}+{y_position}")
+
+# Imposta uno stile per i bottoni
+style = ttk.Style()
+style.configure("TButton", font=("Helvetica", 16))
+
+# Aggiunta del titolo centrato
+title_label = ttk.Label(root, text="CATASTO", font=("Helvetica", 20))
+title_label.pack(pady=20)
 
 # Aggiunta di un pulsante per visualizzare la mappa
-button = ttk.Button(root, text="Visualizza Mappa", command=on_button_click)
-button.pack(pady=20)
+map_button = ttk.Button(root, text="Cerca Punto", command=on_button_click)
+map_button.pack(pady=10)
+
+# Aggiunta di un pulsante per cercare il nome
+search_button = ttk.Button(root, text="Cerca Nome", command=on_search_button_click)
+search_button.pack(pady=10)
+
+# Aggiunta di un pulsante per cercare la strada
+search_newstreet = ttk.Button(root, text="Cerca Strada", command=on_search_button_click)
+search_newstreet.pack(pady=10)
+
+# Aggiunta di un pulsante per uscire dall'applicazione (posizionato in basso a sinistra)
+exit_button = ttk.Button(root, text="Esci", command=on_exit_button_click)
+exit_button.pack(side=tk.BOTTOM, anchor=tk.SW, pady=6)
 
 # Esecuzione della finestra
 root.mainloop()
